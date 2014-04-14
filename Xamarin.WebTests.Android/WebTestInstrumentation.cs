7e26a81cc4095f1c01b56ef38dfba1f4f5dc8e66
Peter Collins <peter.collins@xamarin.com>
2014-04-14 16:20:51 -0400

    [WebTests.Android] Lower target framework to froyo, and add test instrumentation
    The test instrumentation addition allows us to run these tests in an automated fashion from command line. Simply use xbuild (or msbuild) to run the Install and Runtests targets which will respectively install, and then execute the tests on device. RunTests returns the test results to the msbuild output without needing to parse logcat. (e.g. xbuild /t:Install Xamarin.WebTests.Android.csproj; xbuild /t:RunTests Xamarin.WebTests.Android.csproj)

---
+++ Xamarin.WebTests.Android/WebTestInstrumentation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Android.NUnitLite;
+using System.Reflection;
+
+namespace Xamarin.WebTests.Android
+{
+	[Instrumentation]
+	public class WebTestInstrumentation : TestSuiteInstrumentation
+	{
+
+		public WebTestInstrumentation (IntPtr handle, JniHandleOwnership transfer) : base (handle, transfer)
+		{
+		}
+
+		protected override void AddTests ()
+		{
+			AddTest (Assembly.GetExecutingAssembly ());
+		}
+	}
+}
+
