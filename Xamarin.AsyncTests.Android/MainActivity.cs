59212e09bed8bc40fa6da4163a7216ba288d9a3a
Martin Baulig <martin.baulig@xamarin.com>
2014-06-22 22:28:05 +0200

    Create simple Xamarin Forms UI.

---
+++ Xamarin.AsyncTests.Android/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+using Xamarin.AsyncTests.UI;
+using Xamarin.Forms.Platform.Android;
+
+namespace Xamarin.AsyncTests.Android
+{
+	[Activity (Label = "Xamarin.AsyncTests.Android", MainLauncher = true)]
+	public class MainActivity : AndroidActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			Xamarin.Forms.Forms.Init (this, bundle);
+
+			SetPage (App.GetMainPage ());
+		}
+	}
+}
+
+
