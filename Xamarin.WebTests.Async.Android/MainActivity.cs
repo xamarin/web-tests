8a5511654e6187c5a1a6703349fa130d097dc417
Martin Baulig <martin.baulig@xamarin.com>
2014-06-28 00:38:14 +0200

    New work branch, using my AsyncTests framework.

---
+++ Xamarin.WebTests.Async.Android/MainActivity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+using Xamarin.AsyncTests;
+using Xamarin.AsyncTests.UI;
+using Xamarin.AsyncTests.Framework;
+using Xamarin.WebTests.Async;
+
+namespace Xamarin.WebTests.Async.Android
+{
+	[Activity (Label = "Xamarin.WebTests.Async.Android", MainLauncher = true)]
+	public class MainActivity : AndroidActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			Xamarin.Forms.Forms.Init (this, bundle);
+
+			var test = new TestApp ("Xamarin Web Tests");
+			test.LoadAssembly (typeof(SimpleTest).Assembly);
+
+			SetPage (test.Root);
+		}
+	}
+}
+
+
