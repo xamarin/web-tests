5ca60f84da1423bf3a9f5f31d9f890251e183c5a
Martin Baulig <martin.baulig@xamarin.com>
2015-03-03 19:01:07 +0100

    Small fix.

--- Xamarin.AsyncTests.Framework/Xamarin.AsyncTests.Framework.Reflection/ReflectionHelper.cs
+++ Xamarin.AsyncTests.Framework/Xamarin.AsyncTests.Framework.Reflection/ReflectionHelper.cs
@@ -319,7 +319,7 @@ namespace Xamarin.AsyncTests.Framework.Reflection
 
 			var source = Activator.CreateInstance (sourceType);
 			return (ParameterizedTestHost)Activator.CreateInstance (
-				hostType, member.Name, source, serializer, TestFlags.None);
+				hostType, member.Name, source, serializer, null, TestFlags.None);
 		}
 
 		static TestHost CreateBoolean (IMemberInfo member)
