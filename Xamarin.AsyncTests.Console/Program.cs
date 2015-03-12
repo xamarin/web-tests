5da3d1cd14fddab391d0cc4599ce183db4ac2d62
Martin Baulig <martin.baulig@xamarin.com>
2015-03-12 09:59:21 +0100

    Don't reference the samples.

--- Xamarin.AsyncTests.Console/Program.cs
+++ Xamarin.AsyncTests.Console/Program.cs
@@ -139,8 +139,6 @@ namespace Xamarin.AsyncTests.Console
 			Assembly assembly;
 			if (remaining.Count == 1)
 				assembly = Assembly.LoadFile (remaining [0]);
-			else if (remaining.Count == 0)
-				assembly = typeof(Sample.SampleFeatures).Assembly;
 			else
 				throw new InvalidOperationException ();
 
