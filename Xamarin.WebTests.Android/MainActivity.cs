7a050b45ab2c181c68b3dfb0d6da3152f5daad57
Martin Baulig <martin.baulig@xamarin.com>
2015-03-13 13:55:04 +0100

    Initial Android App template.

---
+++ Xamarin.WebTests.Android/MainActivity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Android.App;
+using Android.OS;
+
+namespace Xamarin.WebTests.Android
+{
+	using Forms;
+	using Forms.Platform.Android;
+	using AsyncTests.Mobile;
+
+	[Activity (Label = "Xamarin.WebTests.Android", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : FormsApplicationActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			Forms.Init (this, bundle);
+
+			LoadApplication (new App ());
+		}
+	}
+}
+
