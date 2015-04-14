6de5d0a030eac1d1d7dd82b8ca088db831ed990c
Martin Baulig <martin.baulig@xamarin.com>
2015-04-14 18:03:10 +0200

    Kill test code.

--- Xamarin.WebTests/Xamarin.WebTests.Tests/TestGet.cs
+++ Xamarin.WebTests/Xamarin.WebTests.Tests/TestGet.cs
@@ -111,21 +111,6 @@ namespace Xamarin.WebTests.Tests
 
 			return TestRunner.RunTraditional (ctx, server, redirect, cancellationToken, sendAsync);
 		}
-
-		[Work]
-		[AsyncTest]
-		public void MartinTest (TestContext ctx, CancellationToken cancellationToken, bool test)
-		{
-			ctx.LogMessage ("HELLO WORLD: {0}", test);
-		}
-
-		[Work]
-		[AsyncTest]
-		public void MartinTest2 (TestContext ctx, [GetHandler] Handler handler)
-		{
-			ctx.LogMessage ("HELLO WORLD: {0}", handler);
-		}
-
 	}
 }
 
