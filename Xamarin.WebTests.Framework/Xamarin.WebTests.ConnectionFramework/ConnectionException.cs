d7b4804b5381cb8435c94cf01cd47f327099872a
Martin Baulig <martin.baulig@xamarin.com>
2015-04-27 21:05:33 +0200

    Add ConnectionException.

---
+++ Xamarin.WebTests.Framework/Xamarin.WebTests.ConnectionFramework/ConnectionException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Xamarin.WebTests.ConnectionFramework
+{
+	public class ConnectionException : Exception
+	{
+		public ConnectionException (string message, params object[] args)
+			: base (string.Format (message, args))
+		{
+		}
+	}
+}
+
