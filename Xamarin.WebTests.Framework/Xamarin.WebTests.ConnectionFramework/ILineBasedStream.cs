7136990f951e53e85732548b55451b13b156de78
Martin Baulig <martin.baulig@xamarin.com>
2015-04-28 15:41:30 +0200

    Moved IConnectionHandler and friends.

---
+++ Xamarin.WebTests.Framework/Xamarin.WebTests.ConnectionFramework/ILineBasedStream.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Xamarin.WebTests.ConnectionFramework
+{
+	public interface ILineBasedStream
+	{
+		Task<string> ReadLineAsync ();
+
+		Task WriteLineAsync (string line);
+	}
+}
+
---
+++ Xamarin.WebTests.Framework/Xamarin.WebTests.ConnectionFramework/StreamWrapper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Xamarin.WebTests.ConnectionFramework
+{
+	public class StreamWrapper : ILineBasedStream
+	{
+		public Stream InnerStream {
+			get;
+			private set;
+		}
+
+		StreamReader reader;
+		StreamWriter writer;
+
+		public StreamWrapper (Stream innerStream)
+		{
+			InnerStream = innerStream;
+			reader = new StreamReader (innerStream, Encoding.UTF8);
+			writer = new StreamWriter (innerStream, Encoding.UTF8);
+			writer.AutoFlush = true;
+		}
+
+		public Task<string> ReadLineAsync ()
+		{
+			return reader.ReadLineAsync ();
+		}
+
+		public Task WriteLineAsync (string line)
+		{
+			return writer.WriteLineAsync (line);
+		}
+	}
+}
+
