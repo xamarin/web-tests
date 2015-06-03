ae43aefc8b6f21bd387d4121fb100f4d91493f05
Martin Baulig <martin.baulig@xamarin.com>
2015-06-03 09:00:34 +0200

    Fine-tune protocol versions, distinguishing client and server.

--- Xamarin.WebTests.Framework/Xamarin.WebTests.ConnectionFramework/ProtocolVersions.cs
+++ Xamarin.WebTests.Framework/Xamarin.WebTests.ConnectionFramework/ProtocolVersions.cs
@@ -30,10 +30,22 @@ namespace Xamarin.WebTests.ConnectionFramework
 	[Flags]
 	public enum ProtocolVersions
 	{
-		Default = 0,
-		Tls10 = 192,
-		Tls11 = 768,
-		Tls12 = 3072
+		Default		= 0,
+
+		Tls10Server	= 0x0040,
+		Tls10Client	= 0x0080,
+		Tls10		= (Tls10Server|Tls10Client),
+
+		Tls11Server	= 0x0100,
+		Tls11Client	= 0x0200,
+		Tls11		= (Tls11Server|Tls11Client),
+
+		Tls12Server	= 0x0400,
+		Tls12Client	= 0x0800,
+		Tls12		= (Tls12Server|Tls12Client),
+
+		ServerMask	= (Tls10Server|Tls11Server|Tls12Server),
+		ClientMask	= (Tls10Client|Tls11Client|Tls12Client)
 	}
 }
 
