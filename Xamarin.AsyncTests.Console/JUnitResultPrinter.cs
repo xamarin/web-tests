1ab12742efc772d23b5e7380e2799f81340af0ef
Martin Baulig <mabaul@microsoft.com>
2017-03-22 15:40:20 -0400

    Revert "X"
    This reverts commit e16d17eab558446c2bc3b8daddd1428546ec1361.

--- Xamarin.AsyncTests.Console/JUnitResultPrinter.cs
+++ Xamarin.AsyncTests.Console/JUnitResultPrinter.cs
@@ -94,14 +94,14 @@ namespace Xamarin.AsyncTests.Console
 			var parameterList = "(" + string.Join (",", parameters) + ")";
 			var argumentList = "(" + string.Join (",", values) + ")";
 
-			var (start, end, pos, includeParameters) = GetFormatParameters ();
+			var (start, end, includeParameters) = GetFormatParameters ();
 
 			for (int i = start; i < end; i++) {
 				if (i > start)
 					formatted.Append (".");
-				formatted.Append (parts [i]);
-				if (i == pos)
+				if (i == end - 1)
 					formatted.Append (parameterList);
+				formatted.Append (parts [i]);
 			}
 
 			if (includeParameters) {
@@ -125,21 +125,22 @@ namespace Xamarin.AsyncTests.Console
 				}
 			}
 
-			(int, int, int, bool) GetFormatParameters ()
+			(int, int, bool) GetFormatParameters ()
 			{
 				switch (format) {
 				case NameFormat.Full:
-					return (0, parts.Count, parts.Count, false);
+					return (0, parts.Count, false);
 				case NameFormat.FullWithParameters:
-					return (0, parts.Count, parts.Count, true);
+					return (0, parts.Count, true);
 				case NameFormat.Local:
-					return (parts.Count - 1, parts.Count, -1, false);
+					return (parts.Count - 1, parts.Count, false);
 				case NameFormat.LocalWithParameters:
-					return (parts.Count - 1, parts.Count, -1, true);
+					return (parts.Count - 1, parts.Count, true);
 				case NameFormat.Parent:
-					if (parts.Count == 0)
-						return (0, 1, -1, false);
-					return (0, parts.Count - 1, parts.Count - 1, false);
+					if (parts.Count > 1)
+						return (0, parts.Count - 1, false);
+				else
+					return (0, 1, false);
 				default:
 					throw new InternalErrorException ();
 				}
