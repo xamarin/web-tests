c4dc45880a1c9f8995b16758adc61f7d894cb380
Martin Baulig <mabaul@microsoft.com>
2018-05-02 18:28:36 -0400

    Add IForkedSupport to the DotNet runner.

--- DotNet/Xamarin.WebTests.DotNet/ConsoleMain.cs
+++ DotNet/Xamarin.WebTests.DotNet/ConsoleMain.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Xamarin.AsyncTests;
 using Xamarin.AsyncTests.Console;
+using Xamarin.AsyncTests.Remoting;
 using Xamarin.WebTests.TestProvider;
 using Xamarin.WebTests.ConnectionFramework;
 
@@ -8,18 +9,38 @@ using Xamarin.WebTests.ConnectionFramework;
 
 namespace Xamarin.WebTests.DotNet
 {
-	public class ConsoleMain
+	[Serializable]
+	public class ConsoleMain : IForkedDomainSetup, IForkedSupport
 	{
+		public bool SupportsProcessForks => true;
+
 		static void Main (string[] args)
 		{
+			var main = new ConsoleMain ();
+			main.Run (args);
+		}
+
+		void Run (string[] args)
+		{
+			DependencyInjector.RegisterDependency<IForkedProcessLauncher> (() => new ForkedProcessLauncher ());
+			DependencyInjector.RegisterDependency<IForkedSupport> (() => this);
+			DependencyInjector.RegisterDependency<IForkedDomainSetup> (() => this);
+
+			Initialize ();
+
+			Program.Run (typeof (ConsoleMain).Assembly, args);
+		}
+
+		public void Initialize ()
+		{
 			var setup = new DotNetSetup ();
+			DependencyInjector.RegisterDependency<IForkedSupport> (() => this);
 			DependencyInjector.RegisterDependency<IConnectionFrameworkSetup> (() => setup);
 
-			DependencyInjector.RegisterAssembly (typeof(ConsoleMain).Assembly);
-			DependencyInjector.RegisterAssembly (typeof(WebDependencyProvider).Assembly);
-
-			Program.Run (typeof (WebTestFeatures).Assembly, args);
+			DependencyInjector.RegisterAssembly (typeof (ConsoleMain).Assembly);
+			DependencyInjector.RegisterAssembly (typeof (WebDependencyProvider).Assembly);
 		}
 	}
+
 }
 
